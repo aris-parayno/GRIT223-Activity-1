6e09cf947969d43e07b0989a6e9b2ce3212f1176
aris-parayno <102134742+aris-parayno@users.noreply.github.com>
2023-12-14 17:33:42 +0800

    Update SQL 6.sql

--- SQL 6.sql
+++ SQL 6.sql
@@ -1,3 +1,6 @@
+-- Write a query that returns day, month, year, species for individuals caught in January, May
+-- and July.
+
 -- Select the day, month, year, species columns from the table Rodents_Survey
 -- where the month is 1, 5, or 7
 SELECT
