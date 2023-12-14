dc58ce3468a14e6409938ecb8ba278b37be55726
aris-parayno <102134742+aris-parayno@users.noreply.github.com>
2023-12-14 17:35:41 +0800

    Update SQL 9.sql

--- SQL 9.sql
+++ SQL 9.sql
@@ -1,3 +1,7 @@
+-- Write queries that return: 
+-- 1. How many individuals were counted in each year 
+-- 2. Average weight of each species in each year.
+
 --This query will count the number of distinct species in each year 
 -- and display the result in a table with two columns: year and species_count.
 
