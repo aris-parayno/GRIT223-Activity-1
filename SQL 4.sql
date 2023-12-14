78b42e2057aedb787b3e9c45d7f96036cdbd5179
aris-parayno <102134742+aris-parayno@users.noreply.github.com>
2023-12-14 17:32:44 +0800

    Update SQL 4.sql

--- SQL 4.sql
+++ SQL 4.sql
@@ -1,3 +1,5 @@
+-- Write a query that returns the year, month, day, species and weight in mg.
+
 -- Select the year, month, day, species and weight columns from the table Rodents_Survey
 SELECT
   year,
