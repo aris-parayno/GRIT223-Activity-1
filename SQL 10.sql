901016a421002838997a2f454918d9649914aa11
aris-parayno <102134742+aris-parayno@users.noreply.github.com>
2023-12-14 17:36:31 +0800

    Update SQL 10.sql

--- SQL 10.sql
+++ SQL 10.sql
@@ -1,3 +1,6 @@
+-- Write a query that returns the species, and the weight of every individual captured
+-- at the site
+
 -- This query will calculate the average weight of each species in each year and 
 -- display the result in a table with three columns: year, species, and average_weight.
 
